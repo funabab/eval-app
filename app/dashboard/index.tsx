b57de98d03f7059d6bf2d2a41cdbe6f65705236f
Abdulbassit Abdullahi <funabab@gmail.com>
2023-07-31 04:39:07 +0100

    feat: display latest broadcast in admin dashboard

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -18,12 +18,19 @@ import IconDashboardTimetable from "../../src/components/icons/IconDashboardTime
 import IconDashboardExit from "../../src/components/icons/IconDashboardExit";
 import { useUser } from "../../src/hooks/useUser";
 import dayjs from "dayjs";
+import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
+import { Timestamp, collection, limit, query } from "firebase/firestore";
+import { firebaseFirestore } from "../../src/firebase";
+import Loader from "../../src/components/Loader";
 
 interface Props {}
 
 const DashboardHome: React.FC<Props> = () => {
   const router = useRouter();
   const { user } = useUser();
+  const [broadcast, isLoadingBroadcast] = useCollectionDataOnce(
+    query(collection(firebaseFirestore, "broadcasts"), limit(1))
+  );
 
   const dayPeriod = useMemo(() => {
     const date = dayjs();
@@ -37,6 +44,16 @@ const DashboardHome: React.FC<Props> = () => {
       return "Evening";
     }
   }, []);
+
+  if (isLoadingBroadcast || !broadcast) {
+    return <Loader />;
+  }
+
+  const latestBroadcast = broadcast[0] as {
+    title: string;
+    message: string;
+    createdAt: Timestamp;
+  };
 
   return (
     <View className="flex-1 bg-white">
@@ -56,12 +73,16 @@ const DashboardHome: React.FC<Props> = () => {
 
           <View className="bg-[#D9D9D9] px-7 py-5 mt-6 rounded-lg">
             <Text className="text-xs font-poppins-medium500">
-              School will be closed on Thursday, 24 June 2020, and Friday, 25
-              June 2020 due to the Public Holiday.
+              {latestBroadcast.message}
             </Text>
             <View className="mt-[30px] flex flex-row gap-x-1 justify-center">
               <MaterialCommunityIcons name="clock" size={16} color="black" />
-              <Text className="text-xs">Posted: 25 June 2020</Text>
+              <Text className="text-xs">
+                Posted:{" "}
+                {dayjs(latestBroadcast.createdAt.toDate()).format(
+                  "DD MMMM YYYY"
+                )}
+              </Text>
             </View>
           </View>
 
---
+++ src/components/Loader.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View } from "react-native";
+import AnimatedLottieView from "lottie-react-native";
+import animLoader from "../lottie/animation_loader.json";
+import { styled } from "nativewind";
+
+const StyledAnimatedLottieView = styled(AnimatedLottieView);
+
+interface Props {}
+
+const Loader: React.FC<Props> = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <StyledAnimatedLottieView
+        className="w-52 h-52"
+        source={animLoader}
+        autoPlay
+      />
+    </View>
+  );
+};
+
+export default Loader;
