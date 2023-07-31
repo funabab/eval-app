b07029eab1d87d94e3b2384444e042dbbcb5be60
Abdulbassit Abdullahi <funabab@gmail.com>
2023-07-31 11:30:01 +0100

    fix: evaluation page not scrolling on smaller screen

--- app/dashboard/evaluation/index.tsx
+++ app/dashboard/evaluation/index.tsx
@@ -43,203 +43,207 @@ const EvaluationScreen = () => {
   return (
     <LocationProtect>
       <View className="flex-1 bg-white">
-        {!isVerified && <VerifyIdentity onVerify={() => setIsVerified(true)} />}
+        <ScrollView className="flex-1">
+          {!isVerified && (
+            <VerifyIdentity onVerify={() => setIsVerified(true)} />
+          )}
 
-        {isVerified && (
-          <ScrollView className="flex-1">
-            <Text className="text-center font-poppins-semiboldItalic600">
-              please answer all questions
-            </Text>
-            <View className="flex-1 items-center justify-center pt-2 pb-20 px-7 mt-10">
-              <View className="space-y-8 w-full">
-                <View>
-                  <Text className="font-poppins-medium500 text-base">
-                    Matric Number
-                  </Text>
-                  <Controller
-                    control={control}
-                    name="matricNumber"
-                    render={({
-                      field: { onBlur, onChange, value },
-                      fieldState: { error },
-                    }) => (
-                      <FormControlInput
-                        placeholder="Answer here"
-                        onBlur={onBlur}
-                        onChangeText={onChange}
-                        value={value}
-                        errorMessage={error?.message}
-                      />
-                    )}
-                  />
-                </View>
-
-                <View>
-                  <Text className="font-poppins-medium500 text-base">
-                    Was the lecturer present?
-                  </Text>
-                  <Controller
-                    control={control}
-                    name="lecturerPresence"
-                    render={({ field: { onBlur, onChange, value } }) => (
-                      <View className="rounded-xl border-2 mt-1">
-                        <Picker
+          {isVerified && (
+            <React.Fragment>
+              <Text className="text-center font-poppins-semiboldItalic600">
+                please answer all questions
+              </Text>
+              <View className="flex-1 items-center justify-center pt-2 pb-20 px-7 mt-10">
+                <View className="space-y-8 w-full">
+                  <View>
+                    <Text className="font-poppins-medium500 text-base">
+                      Matric Number
+                    </Text>
+                    <Controller
+                      control={control}
+                      name="matricNumber"
+                      render={({
+                        field: { onBlur, onChange, value },
+                        fieldState: { error },
+                      }) => (
+                        <FormControlInput
                           placeholder="Answer here"
                           onBlur={onBlur}
-                          onValueChange={(item) => onChange(item)}
-                          selectedValue={value}
-                        >
-                          <Picker.Item label="Answer here" enabled={false} />
-                          <Picker.Item label="yes" value="yes" />
-                          <Picker.Item label="no" value="yes" />
-                        </Picker>
-                      </View>
-                    )}
-                  />
-                </View>
-
-                <View>
-                  <Text className="font-poppins-medium500 text-base">
-                    Course code?
-                  </Text>
-                  <Controller
-                    control={control}
-                    name="courseCode"
-                    render={({
-                      field: { onBlur, onChange, value },
-                      fieldState: { error },
-                    }) => (
-                      <FormControlInput
-                        placeholder="Answer here"
-                        onBlur={onBlur}
-                        onChangeText={onChange}
-                        value={value}
-                        errorMessage={error?.message}
-                      />
-                    )}
-                  />
-                </View>
-
-                <View>
-                  <Text className="font-poppins-medium500 text-base">
-                    Rate the lecturer out of 5
-                  </Text>
-                  <Controller
-                    control={control}
-                    name="lecturerRate"
-                    render={({ field: { onBlur, onChange, value } }) => (
-                      <View className="rounded-xl border-2 mt-1">
-                        <Picker
-                          placeholder="Answer here"
-                          onBlur={onBlur}
-                          selectedValue={value}
-                          onValueChange={(item) => onChange(item)}
-                        >
-                          <Picker.Item label="Answer here" enabled={false} />
-                          <Picker.Item label="1" value="1" />
-                          <Picker.Item label="2" value="2" />
-                          <Picker.Item label="3" value="3" />
-                          <Picker.Item label="4" value="4" />
-                          <Picker.Item label="5" value="5" />
-                        </Picker>
-                      </View>
-                    )}
-                  />
-                </View>
-
-                <View>
-                  <Text className="font-poppins-medium500 text-base">
-                    Lecturer’s Name?
-                  </Text>
-                  <Controller
-                    control={control}
-                    name="lecturerName"
-                    render={({
-                      field: { onBlur, onChange, value },
-                      fieldState: { error },
-                    }) => (
-                      <FormControlInput
-                        placeholder="Answer here"
-                        onBlur={onBlur}
-                        onChangeText={onChange}
-                        value={value}
-                        errorMessage={error?.message}
-                      />
-                    )}
-                  />
-                </View>
-
-                <View>
-                  <Text className="font-poppins-medium500 text-base">
-                    Did the lecturer use the necessary material?
-                  </Text>
-                  <Controller
-                    control={control}
-                    name="lecturerMaterial"
-                    render={({ field: { onBlur, onChange, value } }) => (
-                      <View className="rounded-xl border-2 mt-1">
-                        <Picker
-                          placeholder="Answer here"
-                          onBlur={onBlur}
-                          onValueChange={(item) => onChange(item)}
-                          selectedValue={value}
-                        >
-                          <Picker.Item label="Answer here" enabled={false} />
-                          <Picker.Item label="yes" value="yes" />
-                          <Picker.Item label="no" value="yes" />
-                        </Picker>
-                      </View>
-                    )}
-                  />
-                </View>
-
-                <View>
-                  <Text className="font-poppins-medium500 text-base">
-                    Was the lecturer punctual?
-                  </Text>
-                  <Controller
-                    control={control}
-                    name="lecturerPuntuality"
-                    render={({ field: { onBlur, onChange, value } }) => (
-                      <View className="rounded-xl border-2 mt-1">
-                        <Picker
-                          placeholder="Answer here"
-                          onBlur={onBlur}
-                          onValueChange={(item) => onChange(item)}
-                          selectedValue={value}
-                        >
-                          <Picker.Item label="Answer here" enabled={false} />
-                          <Picker.Item label="yes" value="yes" />
-                          <Picker.Item label="no" value="yes" />
-                        </Picker>
-                      </View>
-                    )}
-                  />
-                </View>
-
-                <View>
-                  <TouchableOpacity
-                    className={clsx("mt-2", {
-                      "opacity-60": isLoading,
-                    })}
-                    disabled={isLoading}
-                    onPress={() => onSubmit()}
-                  >
-                    <View className="mx-auto bg-black py-4 px-16 rounded-full">
-                      {isLoading ? (
-                        <ActivityIndicator />
-                      ) : (
-                        <Text className="font-poppins-semibold600 text-white text-sm text-center">
-                          Submit
-                        </Text>
+                          onChangeText={onChange}
+                          value={value}
+                          errorMessage={error?.message}
+                        />
                       )}
-                    </View>
-                  </TouchableOpacity>
+                    />
+                  </View>
+
+                  <View>
+                    <Text className="font-poppins-medium500 text-base">
+                      Was the lecturer present?
+                    </Text>
+                    <Controller
+                      control={control}
+                      name="lecturerPresence"
+                      render={({ field: { onBlur, onChange, value } }) => (
+                        <View className="rounded-xl border-2 mt-1">
+                          <Picker
+                            placeholder="Answer here"
+                            onBlur={onBlur}
+                            onValueChange={(item) => onChange(item)}
+                            selectedValue={value}
+                          >
+                            <Picker.Item label="Answer here" enabled={false} />
+                            <Picker.Item label="yes" value="yes" />
+                            <Picker.Item label="no" value="yes" />
+                          </Picker>
+                        </View>
+                      )}
+                    />
+                  </View>
+
+                  <View>
+                    <Text className="font-poppins-medium500 text-base">
+                      Course code?
+                    </Text>
+                    <Controller
+                      control={control}
+                      name="courseCode"
+                      render={({
+                        field: { onBlur, onChange, value },
+                        fieldState: { error },
+                      }) => (
+                        <FormControlInput
+                          placeholder="Answer here"
+                          onBlur={onBlur}
+                          onChangeText={onChange}
+                          value={value}
+                          errorMessage={error?.message}
+                        />
+                      )}
+                    />
+                  </View>
+
+                  <View>
+                    <Text className="font-poppins-medium500 text-base">
+                      Rate the lecturer out of 5
+                    </Text>
+                    <Controller
+                      control={control}
+                      name="lecturerRate"
+                      render={({ field: { onBlur, onChange, value } }) => (
+                        <View className="rounded-xl border-2 mt-1">
+                          <Picker
+                            placeholder="Answer here"
+                            onBlur={onBlur}
+                            selectedValue={value}
+                            onValueChange={(item) => onChange(item)}
+                          >
+                            <Picker.Item label="Answer here" enabled={false} />
+                            <Picker.Item label="1" value="1" />
+                            <Picker.Item label="2" value="2" />
+                            <Picker.Item label="3" value="3" />
+                            <Picker.Item label="4" value="4" />
+                            <Picker.Item label="5" value="5" />
+                          </Picker>
+                        </View>
+                      )}
+                    />
+                  </View>
+
+                  <View>
+                    <Text className="font-poppins-medium500 text-base">
+                      Lecturer’s Name?
+                    </Text>
+                    <Controller
+                      control={control}
+                      name="lecturerName"
+                      render={({
+                        field: { onBlur, onChange, value },
+                        fieldState: { error },
+                      }) => (
+                        <FormControlInput
+                          placeholder="Answer here"
+                          onBlur={onBlur}
+                          onChangeText={onChange}
+                          value={value}
+                          errorMessage={error?.message}
+                        />
+                      )}
+                    />
+                  </View>
+
+                  <View>
+                    <Text className="font-poppins-medium500 text-base">
+                      Did the lecturer use the necessary material?
+                    </Text>
+                    <Controller
+                      control={control}
+                      name="lecturerMaterial"
+                      render={({ field: { onBlur, onChange, value } }) => (
+                        <View className="rounded-xl border-2 mt-1">
+                          <Picker
+                            placeholder="Answer here"
+                            onBlur={onBlur}
+                            onValueChange={(item) => onChange(item)}
+                            selectedValue={value}
+                          >
+                            <Picker.Item label="Answer here" enabled={false} />
+                            <Picker.Item label="yes" value="yes" />
+                            <Picker.Item label="no" value="yes" />
+                          </Picker>
+                        </View>
+                      )}
+                    />
+                  </View>
+
+                  <View>
+                    <Text className="font-poppins-medium500 text-base">
+                      Was the lecturer punctual?
+                    </Text>
+                    <Controller
+                      control={control}
+                      name="lecturerPuntuality"
+                      render={({ field: { onBlur, onChange, value } }) => (
+                        <View className="rounded-xl border-2 mt-1">
+                          <Picker
+                            placeholder="Answer here"
+                            onBlur={onBlur}
+                            onValueChange={(item) => onChange(item)}
+                            selectedValue={value}
+                          >
+                            <Picker.Item label="Answer here" enabled={false} />
+                            <Picker.Item label="yes" value="yes" />
+                            <Picker.Item label="no" value="yes" />
+                          </Picker>
+                        </View>
+                      )}
+                    />
+                  </View>
+
+                  <View>
+                    <TouchableOpacity
+                      className={clsx("mt-2", {
+                        "opacity-60": isLoading,
+                      })}
+                      disabled={isLoading}
+                      onPress={() => onSubmit()}
+                    >
+                      <View className="mx-auto bg-black py-4 px-16 rounded-full">
+                        {isLoading ? (
+                          <ActivityIndicator />
+                        ) : (
+                          <Text className="font-poppins-semibold600 text-white text-sm text-center">
+                            Submit
+                          </Text>
+                        )}
+                      </View>
+                    </TouchableOpacity>
+                  </View>
                 </View>
               </View>
-            </View>
-          </ScrollView>
-        )}
+            </React.Fragment>
+          )}
+        </ScrollView>
       </View>
     </LocationProtect>
   );
